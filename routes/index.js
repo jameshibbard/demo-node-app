a751579761762fa48f3460ac7397edfd97f76aa3
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2020-01-23 15:32:18 +0100

    Updated for republishing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const path = require('path');
+const auth = require('http-auth');
 const express = require('express');
 const mongoose = require('mongoose');
-const auth = require('http-auth');
-const { body, validationResult } = require('express-validator/check');
+const path = require('path');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
@@ -14,13 +14,12 @@ router.get('/', (req, res) => {
   res.render('form', { title: 'Registration form' });
 });
 
-router.post(
-  '/',
+router.post('/',
   [
-    body('name')
+    check('name')
       .isLength({ min: 1 })
       .withMessage('Please enter a name'),
-    body('email')
+    check('email')
       .isLength({ min: 1 })
       .withMessage('Please enter an email'),
   ],
@@ -31,7 +30,10 @@ router.post(
       const registration = new Registration(req.body);
       registration.save()
         .then(() => { res.send('Thank you for your registration!'); })
-        .catch(() => { res.send('Sorry! Something went wrong.'); });
+        .catch((err) => {
+          console.log(err);
+          res.send('Sorry! Something went wrong.');
+        });
     } else {
       res.render('form', {
         title: 'Registration form',
@@ -39,15 +41,17 @@ router.post(
         data: req.body,
       });
     }
-  }
-);
+  });
 
 router.get('/registrations', auth.connect(basic), (req, res) => {
   Registration.find()
     .then((registrations) => {
       res.render('index', { title: 'Listing registrations', registrations });
     })
-    .catch(() => { res.send('Sorry! Something went wrong.'); });
+    .catch((err) => {
+      console.log(err);
+      res.send('Sorry! Something went wrong.');
+    });
 });
 
 module.exports = router;
--- start.js
+++ start.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
-mongoose.Promise = global.Promise;
+
 mongoose.connection
-  .on('connected', () => {
-    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  .on('open', () => {
+    console.log('Mongoose connection open');
   })
   .on('error', (err) => {
     console.log(`Connection error: ${err.message}`);
